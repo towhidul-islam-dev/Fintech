a6b31c2653af6646306bbfc4ba2e1ddea054deea
towhid <niamaaktar@gmail.com>
2022-09-04 00:38:13 +0600

    all changes has been updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,7 @@ module.exports = {
         "make-equal-cards": "repeat(auto-fit, minmax(5rem, 1fr))",
         "make-equal-cards-1": "repeat(auto-fit, minmax(5rem, 1fr))",
         "make-equal-cards-2": "repeat(auto-fit, minmax(4rem, 1fr))",
-        "pricing-card": "repeat(auto-fit, minmax(10rem, 1fr))",
+        "pricing-card": "repeat(auto-fit, minmax(7rem, 1fr))",
       },
       borderRadius: {
         "custom-radius-1": "6rem",
