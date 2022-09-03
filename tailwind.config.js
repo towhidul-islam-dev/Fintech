5bc7ed8ff29ab48a6661ae8d1c0ba1cd51ab10d9
towhid <niamaaktar@gmail.com>
2022-09-03 16:01:46 +0600

    money section added in the index.html and header section issue has been resolved

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,8 @@ module.exports = {
     extend: {
       gridTemplateColumns: {
         "make-equal-cards": "repeat(auto-fit, minmax(5rem, 1fr))",
-        "make-equal-cards1": "repeat(auto-fit, minmax(5rem, 1fr))",
+        "make-equal-cards-1": "repeat(auto-fit, minmax(5rem, 1fr))",
+        "make-equal-cards-2": "repeat(auto-fit, minmax(4rem, 1fr))",
       },
     },
   },
