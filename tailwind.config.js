ce022c4386629ea9b3f8327086d8104e10f68e2c
towhid <niamaaktar@gmail.com>
2022-09-03 22:37:09 +0600

    pricing section added into the index.html file

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,11 @@ module.exports = {
         "make-equal-cards": "repeat(auto-fit, minmax(5rem, 1fr))",
         "make-equal-cards-1": "repeat(auto-fit, minmax(5rem, 1fr))",
         "make-equal-cards-2": "repeat(auto-fit, minmax(4rem, 1fr))",
+        "pricing-card": "repeat(auto-fit, minmax(14rem, 1fr))",
+      },
+      borderRadius: {
+        "custom-radius-1": "6rem",
+        "custom-radius-round": "50%",
       },
     },
   },
